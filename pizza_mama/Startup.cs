8aa5d0ab91661b661d58a5974ff1dc5f0f57842b
Marise St-Pierre <naranjaqc@hotmail.com>
2023-05-13 21:20:30 -0400

    Change connection string

--- pizza_mama/Startup.cs
+++ pizza_mama/Startup.cs
@@ -26,9 +26,9 @@ namespace pizza_mama {
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => {
                 options.LoginPath = "/Admin";
             });
-            /*services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnectionSqLite")));*/
+            services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnectionSqLite")));
 
-            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionSqlServer")));
+            /*services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionSqlServer")));*/
             services.AddRazorPages();
             services.AddControllers();
         }
